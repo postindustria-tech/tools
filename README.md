edd32bf60790f1bdd2a59aaf67523162a3953a65
Ben51Degrees <ben@51degrees.com>
2023-05-04 14:06:29 +0100

    FEAT: Initial commit of the two main tools used by CI from this repo.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# tools
+# V4 Pipeline Tools
+
+##Property Generation Tool
+The `PropertyGenerationTool` generates code for `DeviceDataBase` and `DeviceData` (`IDeviceData` in .NET) classes for
+device detection Java and .NET using the V4 data file.
+
+See: 
+[DeviceDataBase.java](https://github.com/51Degrees/device-detection-java/blob/master/device-detection.shared/src/main/java/fiftyone/devicedetection/shared/DeviceDataBase.java)
+[DeviceData.java](https://github.com/51Degrees/device-detection-java/blob/master/device-detection.shared/src/main/java/fiftyone/devicedetection/shared/DeviceData.java)
+
+[IDeviceData.cs](https://github.com/51Degrees/device-detection-dotnet/blob/master/FiftyOne.DeviceDetection/FiftyOne.DeviceDetection.Data/Data/IDeviceData.cs)
+[DeviceDataBase.cs](https://github.com/51Degrees/device-detection-dotnet/blob/master/FiftyOne.DeviceDetection/FiftyOne.DeviceDetection.Data/DeviceDataBase.cs)
+
+The License text is kept in the PropertyGenerationTool folder and should contain only the license text.
+
+This is program is run as a part of a daily workflow which rebuilds the device classes from the most recent V4 TAC data file downloaded from the distributor.
---
+++ UpdateLicenseSourceCS/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace UpdateLicenseSourceCS
+{
+    class Program
+    {
+        static string[] excludedirs = { "\\obj\\", "\\bin\\", "\\lib\\", "\\Interop\\Swig\\", "\\.git", "\\.vs" };
+
+        static void Main(string[] args)
+        {
+            var oldcright = new List<string>();
+
+            foreach (var file in Directory.GetFiles(Environment.CurrentDirectory))
+            {
+                if (file.Contains("oldcr") && file.EndsWith(".txt"))
+                    oldcright.Add(File.ReadAllText(file));
+            }
+            //oldcright = null;
+            var cright = File.ReadAllText("copyrightText.txt");
+            RecursiveTraversal("D:\\Workspace\\Pipeline\\pipeline-dotnet", oldcright.ToArray(), cright);
+        }
+
+        private static void RecursiveTraversal(string dir, string[] oldcopyright, string copyright)
+        {
+            var fns = Directory.EnumerateFileSystemEntries(dir);
+            Console.WriteLine("listing " + dir);
+            foreach(var fullfn in fns) {
+                if (excludedirs.Where(e => fullfn.Contains(e)).Count() > 0)
+                    continue;
+                if (Directory.Exists(fullfn))
+                    RecursiveTraversal(fullfn, oldcopyright, copyright);
+                else if (fullfn.EndsWith(".cs"))
+                    UpdateSource(fullfn, oldcopyright, copyright);
+            }
+        }
+
+        private static void UpdateSource(string filename, string[] oldcopyrights, string copyright)
+        {
+            Console.WriteLine("reading " + filename);
+            var fdata = File.ReadAllText(filename);
+
+            if (oldcopyrights != null) {
+                foreach (var oldcopyright in oldcopyrights) {
+                    if (fdata.StartsWith(oldcopyright)) {
+                        fdata = fdata.Substring(oldcopyright.Length).TrimStart();
+                        break;
+                    }
+                }
+            }
+
+            if (fdata.StartsWith(copyright) == false) {
+                Console.WriteLine("updating " + filename);
+                fdata = copyright + fdata;
+                File.WriteAllText(filename, fdata);
+            }
+        }
+    }
+}
+
+    
+
+
+
