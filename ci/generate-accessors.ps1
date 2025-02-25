db0700f90f0ee9a2529cc6fe65b90aa2973d5d24
Alexander Popel <alexander.popel@postindustria.com>
2025-02-25 15:16:11 +0200

    generate-accessors: don't try to guess $DataFile

--- ci/generate-accessors.ps1
+++ ci/generate-accessors.ps1
@@ -1,6 +1,6 @@
 param (
     [Parameter(Mandatory)][string]$RepoName,
-    [string]$DataFile = "$RepoName/TAC-HashV41.hash"
+    [Parameter(Mandatory)][string]$DataFile
 )
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
